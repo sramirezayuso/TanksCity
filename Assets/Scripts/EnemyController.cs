2e37f097418b8245c6928a952bbde27d7642b4ba
tomaslori <tomas_lori142@hotmail.com>
2014-09-24 01:26:27 -0300

    shooting and moving

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,21 +3,77 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+	// Movement
 	public Transform player;
 	public Transform statue;
 	private Transform currentTarget;
 	private NavMeshAgent agent;
 
+	// Shooting
+	public Transform shootingData;
+	public float missileSpeed = 40.00f;
+	public float reloadDelay = 2.0f;
+	private float curReloadTime = 0.0f;
+	private bool reloading = false;
+	public ObjectManagementPool missilePool;
+	
 
+	void Awake() {
+		missilePool = new ObjectManagementPool (Resources.Load ("Prefabs/missile") as GameObject);
+	}
+
+	
 	void Start () {
 		agent = GetComponent<NavMeshAgent> ();
 	}
 
 	void Update () {
+		movementUpdate ();
+		shootingUpdate ();
+	}
+
+	private void movementUpdate () {
 		float distToPlayer = Vector3.Distance (transform.position, player.position);
 		float distToStatue = Vector3.Distance (transform.position, statue.position);
 		currentTarget = (distToPlayer < distToStatue)? player : statue;
-
+		
 		agent.SetDestination (currentTarget.position);
 	}
+
+	private void shootingUpdate () {
+		if(reloading && (curReloadTime += Time.deltaTime) >= reloadDelay) {
+			curReloadTime = 0.0f;
+			reloading = false;
+		}
+
+		bool shouldFire = true;
+		RaycastHit[] hitsInfo = Physics.RaycastAll (transform.position, transform.forward);
+
+		if (!reloading && hitsInfo != null) {
+			
+			foreach (RaycastHit hitInfo in hitsInfo) {
+				if (hitInfo.transform.GetComponent<EnemyController>() != null)
+					shouldFire = false;
+				else if (hitInfo.transform.GetComponent<PlayerController>() != null || hitInfo.transform.GetComponent<StatueController>()) {
+					shouldFire = true;
+					break;
+				}
+			}
+
+			if(shouldFire)
+				shoot();
+			
+		}
+	}
+
+	private void shoot() {
+		GameObject missile = missilePool.getObject (true, shootingData.position, Quaternion.LookRotation(-shootingData.forward));
+		missile.rigidbody.velocity = missileSpeed * shootingData.forward;
+		missile.rigidbody.angularVelocity = new Vector3(20.0f, 0, 0);
+		reloading = true;
+	}
+
+
 }
+
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,35 +3,50 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	private static float movementSpeed = 20.00f;
-	private static float rotationSpeed = 100.00f;
+	// Movement
+	public float movementSpeed = 20.00f;
+	public float rotationSpeed = 100.00f;
 
+	// Shooting
+	public Transform shootingData;
+	public float missileSpeed = 40.00f;
+	public float reloadDelay = 2.0f;
+	private float curReloadTime = 0.0f;
+	private bool reloading = false;
 	private ObjectManagementPool missilePool;
 
 	void Awake() {
 		missilePool = new ObjectManagementPool (Resources.Load ("Prefabs/missile") as GameObject);
 	}
 
-	// Use this for initialization
-	void Start () {
+	void Update () {
+		movementUpdate ();
+		shootingUpdate ();
 	}
 	
-	// Update is called once per frame
-	void Update() {
+	private void movementUpdate () {
 		Vector3 movementVector = transform.forward * Input.GetAxis("Vertical");
 		transform.position += movementVector * movementSpeed * Time.deltaTime;
-
+		
 		Vector3 rotationVector = new Vector3(0, Input.GetAxis("Horizontal"), 0);
 		transform.Rotate (rotationVector * rotationSpeed * Time.deltaTime);
-
-		if(Input.GetKeyDown("space")) {
-			shoot();
+	}
+	
+	private void shootingUpdate () {
+		if(reloading && (curReloadTime += Time.deltaTime) >= reloadDelay) {
+			curReloadTime = 0.0f;
+			reloading = false;
 		}
+
+		if(Input.GetKeyDown("space") && !reloading)
+			shoot();
+
 	}
 
-	void shoot() {
-		Vector3 cameraPosition = transform.Find ("Camera").transform.position;
-		GameObject missile = missilePool.getObject (true, new Vector3(cameraPosition.x, 7.5f, cameraPosition.z) + 10 * transform.forward, Quaternion.LookRotation(-transform.forward));
-		missile.rigidbody.velocity = 40f * transform.forward;
+	private void shoot() {
+		GameObject missile = missilePool.getObject (true, shootingData.position, Quaternion.LookRotation(-shootingData.forward));
+		missile.rigidbody.velocity = missileSpeed * shootingData.forward;
+		missile.rigidbody.angularVelocity = new Vector3(20.0f, 0, 0);
+		reloading = true;
 	}
 }
