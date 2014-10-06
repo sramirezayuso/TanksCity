1f99e72eecf5532d30f84f027c5f3696c6e11c98
tomaslori <tomas_lori142@hotmail.com>
2014-10-06 04:19:22 -0300

    levelbuilder

---
+++ src/Assets/Editor/LevelManagerEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelBuilder))]
+public class LevelBuilderEditor : Editor {
+
+	private bool useRecursiveDivision;
+	private bool useKruskal;
+	private float destroyability;
+	private float completeness;
+	
+	public override void OnInspectorGUI() {
+
+		LevelBuilder myTarget = (LevelBuilder)target;
+
+		DrawDefaultInspector();
+
+		if(GUILayout.Button("Build Level")) {
+
+			myTarget.clear();
+
+			if (useRecursiveDivision)
+				myTarget.buildWithRecursiveDivision();
+			else if (useKruskal)
+				myTarget.buildWithKruskal();
+			else
+				Debug.LogError("This shouldn't happen...");
+
+		}
+
+	}
+	
+}
---
+++ src/Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBuilder : MonoBehaviour {
+
+	public GameObject root;
+
+	public void buildWithRecursiveDivision() {
+
+	}
+
+	public void buildWithKruskal() {
+		
+	}
+
+	public void clear() {
+
+	}
+
+}
